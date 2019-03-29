9400d2fcf71b2cbe97d72293a50ae5babe46fc2e
kingsang <daisuke.toyama@fringe81.com>
2019-03-29 12:38:03 +0900

    headerの許可も入れた

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,8 @@ fn handle_connection(mut stream: TcpStream, path_and_responses: &Vec<PathAndResp
 
   writeln!(stream, "HTTP/1.1 200 OK").unwrap();
   writeln!(stream, "Access-Control-Allow-Origin: *").unwrap();
-  writeln!(stream, "Access-Control-Allow-Methods: GET,POST,HEAD,OPTIONS").unwrap();
+  writeln!(stream, "Access-Control-Allow-Methods: GET,POST,PUT,DELETE,HEAD,OPTIONS").unwrap();
+  writeln!(stream, "Access-Control-Allow-Headers: Origin,Authorization,Accept,Content-Type,x-auth-token").unwrap();
   writeln!(stream, "Content-Type: {}; charset=UTF-8", content_type).unwrap();
   writeln!(stream, "Content-Length: {}", body.len()).unwrap();
   writeln!(stream, "Server: {}", "mockun").unwrap();
